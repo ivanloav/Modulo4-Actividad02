c50b015c3b105c2a4387346c4623f331ccbf3596
ivanloav <ivanloav@gmail.com>
2024-03-19 19:28:14 +0100

    Lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css';
-import { TodoList } from './components/todolist';
+import './App.css';                                 // Importa los estilos de la aplicación
+import { TodoList } from './components/todolist';   // Importa el componente TodoList
 
+// Define y exporta la función App
 function App() {
+  // Renderiza el componente
   return (
     <>
       <h1>Lista de tareas</h1>
